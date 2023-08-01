fa49ed862e7b61a930f16a0e6e66573976c608e7
Can RAZBONYALI <canrazbonyali@gmail.com>
2023-08-01 21:38:30 +0300

    Update OrderApplication.java
    ekleme

--- kafe-backend/order/src/main/java/com/kolaykafe/order/OrderApplication.java
+++ kafe-backend/order/src/main/java/com/kolaykafe/order/OrderApplication.java
@@ -1,15 +1,18 @@
 package com.kolaykafe.order;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@Slf4j
 public class OrderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApplication.class, args);
+		log.info("App started!");
 	}
 
 }
