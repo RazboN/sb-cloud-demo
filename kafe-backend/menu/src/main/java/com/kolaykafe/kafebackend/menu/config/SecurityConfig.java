4b169d98975c86ba62e4de29b6ca84b6a296e6b1
Can Razbonyalı <canrazbonyali@gmail.com>
2023-08-04 10:11:35 +0300

    04.08 eklemeler

---
+++ kafe-backend/menu/src/main/java/com/kolaykafe/kafebackend/menu/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.kolaykafe.kafebackend.menu.config;
+
+import com.kolaykafe.kafebackend.menu.utils.JwtAuthConverter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Autowired
+    private final JwtAuthConverter jwtAuthConverter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated();
+
+        http
+                .oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthConverter);
+
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(STATELESS);
+
+        return http.build();
+    }
+}
